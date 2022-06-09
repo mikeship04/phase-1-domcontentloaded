b4c24729a5296b6f0a9804eb052735b705ead03a
Mike Shippy <mikeship04@gmail.com>
2022-06-09 13:17:35 -0700

    commit tree 52dfb5e97690886bde2d13a401d0048a63ce8345

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    
+    function updateText() {
+        document.getElementById( 'text' )
+        .innerHTML = 'This is really cool!'
+    }
+
+
+    updateText()
+});
+
